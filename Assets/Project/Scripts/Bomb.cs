1e8e423ffd3772e7b30266765bcecd9ad3d1c79a
Erno Newman <ernonewman@gmail.com>
2020-09-14 20:58:44 +0100

    Part 42 - Exploding the bomb

--- Assets/Project/Scripts/Bomb.cs
+++ Assets/Project/Scripts/Bomb.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,14 +7,19 @@ public class Bomb : MonoBehaviour
 {
     public float Duration = 5f;
     public float Radius = 3f;
-
+    public float ExplosionDuration = 0.25f;
+    public GameObject ExplosionModel;
 
     private float _explosionTimer;
+    private bool _exploded;
 
     // Start is called before the first frame update
     void Start()
     {
         _explosionTimer = Duration;
+
+        ExplosionModel.transform.localScale = Vector3.one * Radius;
+        ExplosionModel.SetActive(false);
     }
 
     // Update is called once per frame
@@ -21,9 +27,26 @@ public class Bomb : MonoBehaviour
     {
         _explosionTimer -= Time.deltaTime;
 
-        if (_explosionTimer <= 0)
+        if (_explosionTimer <= 0 && !_exploded)
         {
-            Destroy(gameObject);
+            _exploded = true;
+            var _hitObjects = Physics.OverlapSphere(transform.position, Radius);
+
+            foreach (var collider in _hitObjects)
+            {
+                Debug.Log($"{collider.name} was hit");
+            }
+
+            StartCoroutine(Explode());
         }
+    }
+
+    private IEnumerator Explode()
+    {
+        ExplosionModel.SetActive(true);
+
+        yield return new WaitForSeconds(ExplosionDuration);
+
+        Destroy(gameObject);
     }
 }
