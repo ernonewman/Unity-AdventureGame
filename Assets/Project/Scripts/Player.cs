f9d54a7a163a8341cbefba34c41550340e44175f
Erno Newman <ernonewman@gmail.com>
2020-09-14 16:46:23 +0100

    Part 27 - Moving the player

---
+++ Assets/Project/Scripts/Player.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Net.Sockets;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //transform.position = new Vector3(transform.position.x + 5f * Time.deltaTime, transform.position.y, transform.position.z);
+        //transform.position += new Vector3(5f * Time.deltaTime, 0, 0);
+        transform.position += Vector3.right * 5f * Time.deltaTime;
+    }
+}
