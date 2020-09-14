23742b2fb2ff44e992010ef5e3bdfa107be5c4cb
Erno Newman <ernonewman@gmail.com>
2020-09-14 20:34:19 +0100

    Part 41 - Designing a bomb

---
+++ Assets/Project/Scripts/Bomb.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bomb : MonoBehaviour
+{
+    public float Duration = 5f;
+    public float Radius = 3f;
+
+
+    private float _explosionTimer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _explosionTimer = Duration;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _explosionTimer -= Time.deltaTime;
+
+        if (_explosionTimer <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
