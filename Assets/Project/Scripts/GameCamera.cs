882df1b4f587eb6e34544a58728986dd0bf4c9f5
Erno Newman <ernonewman@gmail.com>
2020-09-16 17:11:54 +0100

    Section 8: Camera and User Interface Part 53 - Camera

---
+++ Assets/Project/Scripts/GameCamera.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameCamera : MonoBehaviour
+{
+    public GameObject Target;
+    public Vector3 Offset;
+    public float FocusSpeed = 1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector3.Lerp(transform.position, Target.transform.position + Offset, Time.deltaTime * FocusSpeed);
+    }
+
+}
